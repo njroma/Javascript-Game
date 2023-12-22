f9821d42fec043ac50ea10d9b342af60cd06932b
njroma <njromaprofessional@gmail.com>
2023-12-23 01:46:05 +0800

    Add LoadScreen in App

---
+++ appscript.js
@@ -0,0 +1,14 @@
+let counter = 0;
+
+var loadingCounter = setInterval(count,1000);
+
+function count(){
+    counter++;
+    console.log(counter); 
+    if (counter >= 12){
+        document.querySelector("#loadScreen").style.display = "none"
+        clearInterval(loadingCounter);
+    }
+}
+
+
--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
 
 let interactionCounter = 0;
-
 let interBtnCounter = document.querySelectorAll(".interactBtn");
 
 interBtnCounter.forEach((interactBtn)=>{
@@ -12,13 +11,18 @@ interBtnCounter.forEach((interactBtn)=>{
         if(interactionCounter == 1){
             document.querySelector("#coachBryl").style.display= "inline";
         }
-        else if(interactionCounter > 6){
+        else if(interactionCounter > 5){
             document.querySelector("#coachBryl").style.display = "none";
+            document.querySelector(".specialBtn").style.display = "none";
             document.querySelector(".playNow").innerHTML = "Play Now!";
+            document.querySelector(".gameStart").innerHTML = `<span data-aos="fade-up" data-aos-easing="ease-out-sine" data-aos-duration="550">
+            Oh no! Let's try to find the bugs 🐞 <br>and be a <span class="extra">developer</span> in <span class="extra">8 weeks.</span></span>`;
+            document.querySelector(".subGameStart").innerHTML = "Click The Button Below To Start! 🎮";
+            document.querySelector(".sub2GameStart").innerHTML = "All you need is a love for finding bugs💖.";
             document.querySelector(".playNow").addEventListener("click", () => {
                 window.location = "/app.html";
             });
         }
     });
-    
 });
+
