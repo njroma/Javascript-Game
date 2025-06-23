cb8ae30af401cf47db9d8628a1d435c2a1873887
njroma <njromaprofessional@gmail.com>
2025-06-24 02:58:26 +0800

    Add Game Container

--- appscript.js
+++ appscript.js
@@ -2,9 +2,10 @@
  
    const video = document.querySelector("#loadScreen");
  
- 
  video.addEventListener('ended', function () {
-    document.querySelector("#introId").style.display = "none"
+    document.querySelector("#introId").style.display = "none";
+    document.querySelector(".appHeader").style.setProperty("z-index", "1", "important");
+    document.querySelector(".heroContainer").style.display = "inline";
   });
 
 
